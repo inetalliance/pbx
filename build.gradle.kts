cf099d61a47de400b36676db9d3ee12a324200f5
Michael Ventura <michael.ventura@gmail.com>
2019-01-31 11:54:53 -0500

    modularized

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,11 @@ apply(plugin= "war")
 
 dependencies {
   compileOnly("javax.servlet:javax.servlet-api:3.1.0")
-  compile(project(":crm:obj"))
-  compile(project(":msg:aj"))
+  if(findProject(":potion") == null) {
+    compile(project(":obj"))
+    compile("net.inetalliance.msg:aj:1.1-SNAPSHOT")
+  } else {
+    compile(project(":crm:obj"))
+    compile(project(":msg:aj"))
+  }
 }

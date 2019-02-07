7a3bd74be38ab786c205d40c693746ade481102d
Michael Ventura <michael.ventura@gmail.com>
2019-02-07 15:30:44 -0500

    add cargo deployment of webapps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 description = "CG2 PBX"
-apply(plugin = "war")
-
+plugins {
+    war
+    id("com.bmuschko.cargo") version "2.5"
+}
 dependencies {
     compile(project(":obj"))
     compileOnly("javax.servlet:javax.servlet-api:3.1.0")
     compile("net.inetalliance.msg:aj:1.1-SNAPSHOT")
 }
+apply(from = "cargo.gradle")

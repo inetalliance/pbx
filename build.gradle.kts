7743d5aa0c52bdaa8f0055d0e23b52fde7ce95b4
Michael Ventura <michael.ventura@gmail.com>
2020-12-07 15:59:20 -0500

    upgrade to gradle 6.7 and fix deprecated dependency scopes

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
   id("com.bmuschko.cargo") version "2.5"
 }
 dependencies {
-  compile(project(":obj"))
-  compile("net.inetalliance.angular:base:1.1-SNAPSHOT")
+  implementation(project(":obj"))
+  implementation("net.inetalliance.angular:base:1.1-SNAPSHOT")
   compileOnly("javax.servlet:javax.servlet-api:3.1.0")
-  compile("net.inetalliance.msg:aj:1.1-SNAPSHOT")
-  runtime("org.postgresql:postgresql:42.2.5")
+  implementation("net.inetalliance.msg:aj:1.1-SNAPSHOT")
+  runtimeOnly("org.postgresql:postgresql:42.2.5")
 }
 apply(from = "cargo.gradle")
 tasks {
